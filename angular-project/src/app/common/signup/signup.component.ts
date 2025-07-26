1c5560e9d32d0d57094df011ee7ebf4bc17649f4
kSadaf14 <khowaja.sadaf14@gmail.com>
2025-07-26 14:56:55 +0500

    Add angular-project/src/app/common/signup/signup.component.ts 1753523810234

---
+++ angular-project/src/app/common/signup/signup.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+  hidePassword = true;
+  hideConfirmPassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.passwordMatchValidator });
+  }
+
+  ngOnInit(): void {}
+
+  passwordMatchValidator(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    
+    if (password && confirmPassword && password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ passwordMismatch: true });
+    } else {
+      if (confirmPassword?.hasError('passwordMismatch')) {
+        confirmPassword.setErrors(null);
+      }
+    }
+    return null;
+  }
+
+  onSubmit(): void {
+    if (this.signupForm.valid) {
+      console.log('Signup form submitted:', this.signupForm.value);
+      // Add signup logic here
+    }
+  }
+
+  getErrorMessage(field: string): string {
+    const control = this.signupForm.get(field);
+    if (control?.hasError('required')) {
+      return `${field.charAt(0).toUpperCase() + field.slice(1)} is required`;
+    }
+    if (control?.hasError('email')) {
+      return 'Please enter a valid email';
+    }
+    if (control?.hasError('minlength')) {
+      const requiredLength = control.errors?.['minlength']?.requiredLength;
+      return `${field.charAt(0).toUpperCase() + field.slice(1)} must be at least ${requiredLength} characters`;
+    }
+    if (control?.hasError('passwordMismatch')) {
+      return 'Passwords do not match';
+    }
+    return '';
+  }
+}
