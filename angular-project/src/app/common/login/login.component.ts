e0e9a95bbca9f7b10a9b64cfde6d12aa06978a50
kSadaf14 <khowaja.sadaf14@gmail.com>
2025-07-26 14:57:00 +0500

    Add angular-project/src/app/common/login/login.component.ts 1753523816240

---
+++ angular-project/src/app/common/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login form submitted:', this.loginForm.value);
+      // Add login logic here
+    }
+  }
+
+  getErrorMessage(field: string): string {
+    const control = this.loginForm.get(field);
+    if (control?.hasError('required')) {
+      return `${field.charAt(0).toUpperCase() + field.slice(1)} is required`;
+    }
+    if (control?.hasError('email')) {
+      return 'Please enter a valid email';
+    }
+    if (control?.hasError('minlength')) {
+      return 'Password must be at least 6 characters';
+    }
+    return '';
+  }
+}
